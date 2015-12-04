2f3406aa664e799a760fb78397c44448e4c25706
Patryk Gurgul <patrykgurgul.it@gmail.com>
2015-12-04 15:01:29 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/js/main.js',
+  output: {
+    path: __dirname + '/dist/js/',
+    filename: 'build.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+};
